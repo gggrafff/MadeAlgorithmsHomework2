3daa0f107dd6d1117a2ecd3377442198d4fcbb89
andargunov <tolik_azo@mail.ru>
2020-03-02 01:37:57 +0300

    hw2. tasks 1 and 2 is done.

--- 2_dynamics_advanced/task1/main.cpp
+++ 2_dynamics_advanced/task1/main.cpp
@@ -1,43 +1,142 @@
 /*
- * 
+ * A. Разбиение на пары
+ * ограничение по времени на тест 2 секунды
+ * ограничение по памяти на тест 64 мегабайта
+ * ввод стандартный ввод
+ * вывод стандартный вывод
+
+ * Однажды n ребят решили поучаствовать в VK Cup. Для этого им нужно разбиться на команды по два человека.
+ * Однако выяснилось, что не все ребята хорошо ладят друг с другом. Вы выяснили для каждой пары ребят,
+ * могут ли они участвовать в одной команде. Помогите им сформировать максимальное число команд.
+ *
+ * Входные данные
+ * Первая строка содержит число n (1 ≤ n ≤ 18), далее n строк по n символов описывают отношения ребят.
+ * Символ j строки i равен «Y», если ребята i и j могут участвовать в команде.
+ * Выходные данные
+ * Выведите одно число — максимальное число участников в командах, которые можно сформировать.
+ *
+ * Примеры
+ * Входные данные
+ * 5
+ * NYYYY
+ * YNNNN
+ * YNNNN
+ * YNNNN
+ * YNNNN
+ * Выходные данные
+ * 2
+ *
+ * Входные данные
+ * 5
+ * NYYYY
+ * YNNNN
+ * YNNNY
+ * YNNNY
+ * YNYYN
+ * Выходные данные
+ * 4
  */
 
- /*
-  * Что храним? dp[mask] - максимальный ответ для подмножества mask
-  * База dp[0] = 0
-  * Переход dp[mask] = max(dp[mask'], dp[mask''] + 1), где mask' = mask - 2^i, mask'' = mask - 2^i - 2^j (есть ребро между i и j)
-  * Порядок по возрастанию mask
-  * Ответ dp[2^n - 1]
-  */
+/*
+ * Что храним? dp[mask] - максимальный ответ для подмножества mask
+ * База dp[0] = 0
+ * Переход dp[mask] = max(dp[mask'], dp[mask''] + 1), где mask' = mask - 2^i, mask'' = mask - 2^i - 2^j (есть ребро между i и j)
+ * Порядок по возрастанию mask
+ * Ответ dp[2^n - 1]
+ */
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-using Graph = std::vector<std::vector<size_t>>;
+/**
+ * @brief Матрица смежности графа.
+ */
+using Graph = std::vector<std::vector<bool>>;
 
-uint64_t find_highest_matching(const Graph& graph) {
-    std::vector<int64_t> dp(N, 0);
-    std::vector<size_t> parents(1, 0);
-    for (size_t i = 1; i < N; ++i) {
-        auto max_profit_point = std::max_element(next(dp.begin(), (i > K ? i - K : 0)), next(dp.begin(), i));
-        dp[i] = coins[i] + *max_profit_point;
-        auto distance = std::distance(dp.begin(), max_profit_point);
-        parents.push_back((distance > 0 ? static_cast<size_t>(distance) : 0));
-    }
-    std::vector<size_t> path(1, N-1);
-    auto current_index = N - 1;
-    while(parents[current_index] != current_index)
-    {
-        path.push_back(parents[current_index]);
-        current_index = parents[current_index];
-    }
-    std::reverse(path.begin(), path.end());
-    return {path, dp[N - 1]};
+/**
+ * @brief Извлечение значения бита из битовой маски.
+ * @tparam NumberType Числовой тип данных, которым представлена битовая маска.
+ * @param mask Битовая маска.
+ * @param index Номер бита.
+ * @return Значение бита.
+ */
+template<typename NumberType>
+NumberType getBit(NumberType mask, uint8_t index)
+{
+    return (mask >> index) & 1;
 }
 
+/**
+ * @brief Поиск максимального паросочетания в произвольном графе методом ДП.
+ * @param graph Матрица смежности графа. Количество вершин графа не более 30.
+ * @return Максимальное паросочетание.
+ */
+uint64_t find_highest_matching(const Graph &graph) {
+    assert(graph.size() <= 30);
+    size_t N = (1U << graph.size());  // количество всех подмножеств
+    std::vector<uint64_t> dp(N, 0);
+    for (size_t mask = 1; mask < N; ++mask) {
+        for (size_t i = 0; i < graph.size(); ++i)
+        {
+            if (getBit(mask, static_cast<uint8_t>(i)) == 1) {
+                dp[mask] = dp[mask - (1 << i)];  // mask' = mask - 2^i
+                for (size_t j = 0; j < graph.size(); ++j) {
+                    if (getBit(mask, static_cast<uint8_t>(j)) == 1 && graph[i][j]) {
+                        dp[mask] = std::max(dp[mask], dp[mask - (1 << i) - (1 << j)] + 1);  // mask'' = mask - 2^i - 2^j
+                    }
+                }
+            }
+        }
+    }
+    return dp.back();
+}
+
+// Начало тестов
+
+void test_from_task_1() {
+    Graph graph = {
+            {false, true, true, true, true},
+            {true, false, false, false, false},
+            {true, false, false, false, false},
+            {true, false, false, false, false},
+            {true, false, false, false, false},
+    };
+    auto result = find_highest_matching(graph);
+    assert(result == 1);
+}
+
+void test_from_task_2() {
+    Graph graph = {
+            {false, true, true, true, true},
+            {true, false, false, false, false},
+            {true, false, false, false, true},
+            {true, false, false, false, true},
+            {true, false, true, true, false},
+    };
+    auto result = find_highest_matching(graph);
+    assert(result == 2);
+}
+
+void run_all_tests()
+{
+    test_from_task_1();
+    test_from_task_2();
+}
+
+// Конец тестов
+
 int main(int argc, char *argv[]) {
+    if (argc > 1)
+    {
+        if(std::string(argv[1]) == "test")  // запуск тестов
+        {
+            run_all_tests();
+            return 0;
+        }
+    }
+
     // Чтение входных данных
     size_t N{0};
     std::cin >> N;
@@ -45,17 +144,16 @@ int main(int argc, char *argv[]) {
     for (size_t i = 0; i < N; ++i) {
         std::string compatibility;
         std::cin >> compatibility;
-        for(size_t j = 0; j < N; ++j) {
-        	if (compatibility[j] == 'Y') {
-        		
-        	}
+        graph[i].reserve(N);
+        for (size_t j = 0; j < N; ++j) {
+            graph[i].push_back(compatibility[j] == 'Y');
         }
     }
-    
+
     auto pairs_number = find_highest_matching(graph);
 
     // Запись результата
-    std::cout << pairs_number * 2;  // 
+    std::cout << pairs_number * 2;  // Вывести нужно количество участников (вдвое больше количества пар)
 
     return 0;
 }
---
+++ 2_dynamics_advanced/task2/main.cpp
@@ -0,0 +1,225 @@
+/*
+ * B. Удаление скобок 2.0
+ * ограничение по времени на тест 2 секунды
+ * ограничение по памяти на тест 256 мегабайт
+ * ввод стандартный ввод
+ * вывод стандартный вывод
+
+ * Дана строка, составленная из круглых, квадратных и фигурных скобок.
+ * Определите, какое наименьшее количество символов необходимо удалить из этой строки,
+ * чтобы оставшиеся символы образовывали правильную скобочную последовательность.
+ *
+ * Входные данные
+ * Во входном файле записана строка из круглых, квадратных и фигурных скобок.
+ * Длина строки не превосходит {100} символов.
+ * Выходные данные
+ * Выведите строку максимальной длины, являющейся правильной скобочной последовательностью,
+ * которую можно получить из исходной строки удалением некоторых символов.
+ *
+ * Пример
+ * Входные данные
+ * ([)]
+ * Выходные данные
+ * []
+ */
+
+/*
+ * Что храним? dp[i][j] - минимальный ответ на отрезке [i, j]
+ * База dp[i][i] = 1
+ * Переход dp[i][j] = min(dp[i][k] + dp[k][j], dp[i+1][j-1] (если концы - корректные взаимные скобочки)), k=i..j
+ * Порядок по возрастанию длины
+ * Ответ dp[0][n]
+ */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <limits>
+
+/**
+ * @brief Восстановление ответа. Рекурсиваня функция.
+ * @param i Левый конец отрезка.
+ * @param j Правый конец отрезка.
+ * @param dp Динамика.
+ * @param separators Разделители отрезков на подотрезки.
+ * @param brackets Исходная строка.
+ * @return Наиболее длинная корректная строка со скобками, полученная из исходной удалением символов.
+ */
+std::string restore_answer(
+        size_t i,
+        size_t j,
+        const std::vector<std::vector<size_t>> &dp,
+        const std::vector<std::vector<ssize_t>> &separators,
+        const std::string &brackets) {
+    assert(j>=i);
+    if (dp[i][j] == j - i + 1) {  // если все символы плохие
+        return "";
+    }
+    if (dp[i][j] == 0) {  // если все символы хорошие (строка полностью корректна)
+        return brackets.substr(i, j - i + 1);
+    }
+    if (separators[i][j] == -1) {  // особый случай типа (...)
+        return brackets[i] + restore_answer(i + 1, j - 1, dp, separators, brackets) + brackets[j];
+    }
+    // остальные случаи: отрезок делится на подотрезки
+    auto left_part = restore_answer(i, static_cast<size_t>(separators[i][j]), dp, separators, brackets);
+    auto right_part = restore_answer(static_cast<size_t>(separators[i][j] + 1), j, dp, separators, brackets);
+    return left_part + right_part;
+}
+
+/**
+ * @brief Поиск наиболее длинной корректной последовательности скобок в строке методом ДП.
+ * @param brackets Строка со скобками.
+ * @return Наиболее длинная корректная строка со скобками, полученная из исходной удалением символов.
+ */
+std::string find_longest_valid_brackets(const std::string &brackets) {
+    if (brackets.empty())
+    {
+        return "";
+    }
+
+    std::vector<std::vector<size_t>> dp(
+            brackets.size(),
+            std::vector<size_t>(brackets.size(), std::numeric_limits<size_t>::max()));
+    std::vector<std::vector<ssize_t>> separators(  // массив разделителей отрезка на подотрезки, из которых он получен
+            brackets.size(),
+            std::vector<ssize_t>(
+                    brackets.size(),
+                    std::numeric_limits<ssize_t>::max()));
+
+    for (size_t i = 0; i < brackets.size(); ++i) {
+        dp[i][i] = 1;  // база
+    }
+
+    for (size_t length = 2; length <= brackets.size(); ++length) {  // по возрастанию длины
+        for (size_t i = 0; i <= brackets.size() - length; ++i) {  // итерируемся по левому концу отрезка
+            auto j = i + length - 1;  // правый конец отрезка
+            assert(j > i);
+            for (size_t k = i; k < j; ++k) {  // ищем такое деление отрезка на подотрезки, которое даст наилучший результат
+                auto sum = dp[i][k] + dp[k + 1][j];
+                if (sum < dp[i][j]) {
+                    dp[i][j] = sum;
+                    assert(k >= i);
+                    separators[i][j] = k;  // запоминаем, как поделили отрезок на подотрезки
+                }
+            }
+            // отдельно рассмотрим случай типа (...), когда корректное выражение корректно вложено в скобочки
+            if ((brackets[i] == '(' && brackets[j] == ')') ||
+                (brackets[i] == '[' && brackets[j] == ']') ||
+                (brackets[i] == '{' && brackets[j] == '}')) {
+                assert(j > i);
+                if (j > i + 1) {  // если длина подотрезка не нулевая
+                    if (dp[i + 1][j - 1] < dp[i][j])
+                    {
+                        dp[i][j] = dp[i + 1][j - 1];
+                        separators[i][j] = -1;  // помечаем, что случай особый
+                    }
+                } else {
+                    dp[i][j] = 0;  // для нулевой длины подотрезка
+                    separators[i][j] = -1;  // помечаем, что случай особый
+                }
+            }
+        }
+    }
+
+    return restore_answer(0, brackets.size() - 1, dp, separators, brackets);  // восстанавливаем ответ рекурсивно
+}
+
+// Начало тестов
+
+void test_from_task_1() {
+    std::string input = "([)]";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "[]");
+}
+
+void test_by_shujkova_1() {
+    std::string input = "({{)}}";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "{{}}");
+}
+
+void test_by_shujkova_2() {
+    std::string input = "{([(]{)})]";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "[({})]");
+}
+
+void test_good_brackets() {
+    std::string input = "[({})]";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "[({})]");
+}
+
+void test_empty_string() {
+    std::string input = "";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "");
+}
+
+void test_all_incorrect() {
+    std::string input = ")}}{]([({(";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "");
+}
+
+void test_nested_valid_brackets() {
+    std::string input = "{{}({}()[])[]}([{}])[]";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "{{}({}()[])[]}([{}])[]");
+}
+
+void test_nested_invalid_brackets() {
+    std::string input = "{{}(gf{}(s)[]f)[j]}e([{g}])[]g";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "{{}({}()[])[]}([{}])[]");
+}
+
+void test_one_bracket() {
+    std::string input = "{";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "");
+}
+
+void test_megatits() {
+    std::string input = "())(()";
+    auto result = find_longest_valid_brackets(input);
+    assert(result == "()()");
+}
+
+void run_all_tests() {
+    test_from_task_1();
+    test_by_shujkova_1();
+    test_by_shujkova_2();
+    test_good_brackets();
+    test_empty_string();
+    test_all_incorrect();
+    test_nested_valid_brackets();
+    test_nested_invalid_brackets();
+    test_one_bracket();
+    test_megatits();
+}
+
+// Конец тестов
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        if (std::string(argv[1]) == "test")  // запуск тестов
+        {
+            run_all_tests();
+            return 0;
+        }
+    }
+
+    // Чтение входных данных
+    std::string brackets;
+    std::cin >> brackets;
+
+    auto correct_brackets = find_longest_valid_brackets(brackets);
+
+    // Запись результата
+    std::cout << correct_brackets;
+
+    return 0;
+}
