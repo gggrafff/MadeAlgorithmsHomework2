a9dcebc35264ade7b9fc93472037cb576764bcb3
andargunov <tolik_azo@mail.ru>
2020-03-02 23:31:34 +0300

    hw2 task4 in progress

--- 2_dynamics_advanced/task4/main.cpp
+++ 2_dynamics_advanced/task4/main.cpp
@@ -33,7 +33,7 @@
  * База dp[start][0] = 0; 
  * Переход dp[v][mask] = min{ dp[u][mask - 2^u] + length[u][v] } по всем u из mask
  * Порядок по возрастанию mask
- * Ответ dp[start][2^n - 1]
+ * Ответ min{dp[start][2^n - 1 - 1]}
  */
 
 #include <iostream>
@@ -54,84 +54,111 @@ using Path = std::pair<uint64_t, std::vector<size_t>>;
  */
 template<typename NumberType>
 NumberType getBit(NumberType mask, uint8_t index) {
-  return (mask >> index) & 1;
+    return (mask >> index) & 1;
 }
 
-Path find_shortest_path(const Graph& graph) {
-  assert(graph.size() <= 30);
-  
-  const size_t N = (static_cast<size_t>(1) << graph.size());
-  std::vector<std::vector<uint64_t>> dp(graph.size(), std::vector<uint64_t>(N, std::numeric_limits<uint32_t>::max()));
-  std::vector<std::vector<size_t>> parents(graph.size(), std::vector<size_t>(N, 0));
-  
-  for (size_t start = 0; start < graph.size(); ++start) {
-    dp[start][0] = 0;
-  }
-  
-  for(uint32_t mask = 1; mask < N; ++mask) {
-    for (size_t v = 0; v < graph.size(); ++v) {
-      for (size_t u = 0; u < graph.size(); ++u) {
-        if (getBit(mask, u) == 1) {
-          const auto length = graph[u][v] + dp[u][mask - (1 << u)];
-          if (length < dp[v][mask]) {
-            dp[v][mask] = length;
-            parents[v][mask] = u;
-          }
-        }
-      }
-    }
-  }
+Path find_shortest_path(const Graph &graph) {
+    assert(graph.size() <= 30);
 
-  Path path;
-  path.first = dp[0][N - 1];
-  /*for (auto& elem: dp)
-  {
-  	for (auto& elem2: elem)
-  {
-  	std::cout << elem2 << " ";
-  }
-  std::cout << std::endl;
-  }*/
-  size_t current_index = 0;
-  uint32_t current_mask = N-1;
-  while (current_mask!=0){
-  	path.second.push_back(parents[current_index][current_mask]);
-  	current_index = parents[current_index][current_mask];
-  	current_mask -= 1 << current_index;
-  }
-  return path;
+    const size_t N = (static_cast<size_t>(1) << graph.size());
+    std::vector<std::vector<uint64_t>> dp(graph.size(), std::vector<uint64_t>(N, std::numeric_limits<uint32_t>::max()));
+    std::vector<std::vector<size_t>> parents(graph.size(), std::vector<size_t>(N, 0));
+
+    for (size_t start = 0; start < graph.size(); ++start) {
+        dp[start][0] = 0;
+    }
+
+    for (uint32_t mask = 1; mask < N; ++mask) {
+        for (size_t v = 0; v < graph.size(); ++v) {
+            for (size_t u = 0; u < graph.size(); ++u) {
+                if (u == v)
+                {
+                    continue;
+                }
+                if (getBit(mask, static_cast<uint8_t>(u)) == 1) {
+                    const auto length = graph[u][v] + dp[u][mask - (1 << u)];
+                    if (length < dp[v][mask]) {
+                        dp[v][mask] = length;
+                        parents[v][mask] = u;
+                    }
+                }
+            }
+        }
+    }
+
+    Path path;
+    auto last_city_index = 0;
+    for (auto i = 1; i < dp.size();++i)
+    {
+        if (dp[i][N-1-(1<<i)] < dp[last_city_index][N-1-(1<<last_city_index)])
+        {
+            last_city_index = i;
+        }
+    }
+    path.first = dp[last_city_index][N-1-(1<<last_city_index)];
+
+    auto current_index = last_city_index;
+    path.second.push_back(static_cast<size_t>(current_index));
+    uint32_t current_mask = static_cast<uint32_t>(N) - 1 - (1 << current_index);
+    while (current_mask != 0) {
+        path.second.push_back(parents[current_index][current_mask]);
+        current_index = parents[current_index][current_mask];
+        current_mask -= 1 << current_index;
+    }
+    auto control_length = 0;
+    for(auto i = 0; i<path.second.size()-1;++i)
+    {
+        control_length += graph[path.second[i]][path.second[i+1]];
+    }
+    assert(path.first == control_length);
+    return path;
 }
 
 // Начало тестов
 
 void test_from_task_1() {
-  const Graph graph { 
-    {0, 183, 163, 173, 181},
-    {183, 0, 165, 172, 171 },
-    {163, 165, 0, 189, 302 },
-    {173, 172, 189, 0, 167},
-    {181, 171, 302, 167, 0}, };
-  const auto path = find_shortest_path(graph);
-  std::cout << path.first<<std::endl;
-  for (auto& city: path.second){
-  	std::cout<<city<<" ";
-  }
-  //assert(path.first == 666);
+    const Graph graph{
+            {0,   183, 163, 173, 181},
+            {183, 0,   165, 172, 171},
+            {163, 165, 0,   189, 302},
+            {173, 172, 189, 0,   167},
+            {181, 171, 302, 167, 0},};
+    const auto path = find_shortest_path(graph);
+    assert(path.first == 666);
 }
 
-void run_all_tests()
-{
+void test_one_city() {
+    const Graph graph{
+            {0},};
+    const auto path = find_shortest_path(graph);
+    assert(path.first == 0);
+    assert(path.second == std::vector<size_t>{0});
+}
+
+void test_by_saturn_team() {
+    const Graph graph{
+            {0,   20, 18, 12, 8},
+            {5, 0,   14, 7, 11},
+            {12, 18, 0,   6, 11},
+            {11, 17, 11, 0,   12},
+            {5, 5, 5, 5, 0},};
+    const auto path = find_shortest_path(graph);
+    assert(path.first == 36);
+}
+
+void run_all_tests() {
     test_from_task_1();
+    test_one_city();
+    test_by_saturn_team();
 }
 
 // Конец тестов
 
 int main(int argc, char *argv[]) {
-  run_all_tests();
-  return 0;
-    if (argc > 1)
-    {
-        if(std::string(argv[1]) == "test")  // запуск тестов
+    run_all_tests();
+    return 0;
+    if (argc > 1) {
+        if (std::string(argv[1]) == "test")  // запуск тестов
         {
             run_all_tests();
             return 0;
@@ -139,13 +166,13 @@ int main(int argc, char *argv[]) {
     }
 
     // Чтение входных данных
-    size_t N{ 0 };
+    size_t N{0};
     std::cin >> N;
-    Graph graph(N, {N, 0});
-    for (size_t i = 0; i< N; ++i) {
-      for (size_t j = 0; j < N; ++j) {
-        std::cin >> graph[i][j];
-      }
+    Graph graph(N, std::vector<uint64_t>(N, 0));
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            std::cin >> graph[i][j];
+        }
     }
 
     auto path = find_shortest_path(graph);
@@ -153,7 +180,7 @@ int main(int argc, char *argv[]) {
     // Запись результата
     std::cout << path.first << std::endl;
     for (auto city : path.second) {
-      std::cout << city + 1 << " ";
+        std::cout << city + 1 << " ";
     }
 
     return 0;
